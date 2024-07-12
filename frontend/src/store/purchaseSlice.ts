d22e849d5c905a6a0ef4b1e16917570b659f956c
Min Htet Oo <minhtetoo.dev@gmail.com>
2024-07-12 17:41:25 +0800

    feat: purchase history

---
+++ frontend/src/store/purchaseSlice.ts
@@ -0,0 +1,98 @@
+// src/features/purchase/purchaseSlice.ts
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { Purchase } from "@/types";
+import { RootState } from "./store";
+import axios from "axios";
+
+export const fetchPurchases = createAsyncThunk(
+  "purchase/fetchPurchases",
+  async (_, { getState }) => {
+    const { auth } = getState() as RootState;
+    const token = auth.token;
+    console.log("before fetch");
+    const response = await axios.get(
+      `http://localhost:8080/purchases?buyer_id=${auth.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return response.data;
+  }
+);
+
+export const fetchSales = createAsyncThunk(
+  "purchase/fetchSales",
+  async (_, { getState }) => {
+    const { auth } = getState() as RootState;
+    const token = auth.token;
+    const response = await axios.get(
+      `http://localhost:8080/purchases?seller_id=${auth.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  }
+);
+
+interface PurchaseState {
+  purchases: Purchase[];
+  sales: Purchase[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: PurchaseState = {
+  purchases: [],
+  sales: [],
+  loading: false,
+  error: null,
+};
+
+const purchaseSlice = createSlice({
+  name: "purchase",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPurchases.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchPurchases.fulfilled,
+        (state, action: PayloadAction<Purchase[]>) => {
+          state.loading = false;
+          state.purchases = action.payload;
+        }
+      )
+      .addCase(fetchPurchases.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch purchases";
+      })
+      .addCase(fetchSales.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchSales.fulfilled,
+        (state, action: PayloadAction<Purchase[]>) => {
+          state.loading = false;
+          state.sales = action.payload;
+        }
+      )
+      .addCase(fetchSales.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch sales";
+      });
+  },
+});
+
+export default purchaseSlice.reducer;
